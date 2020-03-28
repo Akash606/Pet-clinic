2d6dadbbe620d8ccb6cd545d74131cc2d8c24c34
Akash Sah <sahakash2017@gmail.com>
2020-03-28 09:14:32 +0545

    inital Project add

---
+++ src/test/java/com/aksh/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksh.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
